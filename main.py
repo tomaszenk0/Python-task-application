beb3f9300808540feede3dae7a2b3e31f89cc9b7
tomaszenk0 <tomeksza.ts@gmail.com>
2026-06-01 21:08:42 +0200

    Adding numpy taksk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-np.random.seed(42)
-A = np.random.randint(10, size=(100, 30))
-np.set_printoptions(edgeitems=10)
-print(A)
+A = np.array([[5, 8, 16],
+              [4, 1, 8],
+              [-4, 4, -11]])
+print(np.trace(A))
