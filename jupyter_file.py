fc4d738a915b55f56932189ebefe238296eb72fa
tomaszenk0 <tomeksza.ts@gmail.com>
2026-09-05 19:18:14 +0200

    adding-new-version

--- jupyter_file.py
+++ jupyter_file.py
@@ -2,43 +2,53 @@ import datetime
 import nbformat
 import json
 import random
+import numpy as np
 
-nb=nbformat.read("Zadania.ipynb" , as_version=4)
-content=""
+files=["Zadania.ipynb", 'Automotive_TASK.ipynb']
+notebooks=[]
+
+total_tasks=0
+
+for file_path in files:
+    nb=nbformat.read(file_path, as_version=4)
+    notebooks.append(nb)
+    total_tasks += sum(1 for cell in nb.cells if cell.cell_type == 'markdown')
 
 
-total_tasks=sum(1 for cell in nb.cells if cell.cell_type=='markdown')
 
 start_date=datetime.datetime(2026, 1, 1)
-end_date=datetime.datetime(2026, 12,31)
-
+end_date=datetime.datetime(2027, 1,1)
 total_days=(end_date-start_date).days
-avg_step=total_days/max(total_tasks,1)
 
-min_step=0
-max_step=max(1, int(avg_step*1.8))
 
-current_date=start_date
-
-task_number=-1
+task_number=0
 result=[]
-for cell in nb.cells:
-    if cell.cell_type=="markdown":
-        task_number+=1
-        content=cell.source.replace("\n", " ")
 
-        date_str = current_date.strftime("%Y-%m-%d")
-        delta_days = random.randint(min_step, max_step)
-        current_date += datetime.timedelta(days=delta_days)
-        if current_date>end_date:
-            current_date=end_date
+for nb in notebooks:
+    for cell in nb.cells:
+        if cell.cell_type == "markdown":
+            task_number += 1
+            content = cell.source.replace("\n", " ")
+            if total_tasks>1:
+                fraction=task_number/(total_tasks-1)
+            else:
+                fraction=0
 
-        task_name = f"Zadanie {task_number}"
+            base_days=int(fraction*total_days)
 
-    else:
-        solve= cell.source.strip()
-        task_dict = {"task": task_name, "content": content, "solve": solve, "date": date_str}
-        result.append(task_dict)
+            actual_days = int(fraction * total_days)
+
+            task_date=start_date+datetime.timedelta(days=actual_days)
+            date_str = task_date.strftime("%Y-%m-%d")
+
+            task_name = f"Zadanie {task_number}"
+
+        else:
+            solve = cell.source.strip()
+            task_dict = {"task": task_name, "content": content, "solve": solve, "date": date_str}
+            result.append(task_dict)
+
+
 
 with open("tasks.json", "w", encoding="utf-8") as f:
     json.dump(result, f, ensure_ascii=False, indent=2)
--- main-page.py
+++ main-page.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import json
 import time
+import matplotlib.pyplot as plt
+import plotly.express as px
+import re
+from collections import Counter
+import pandas as pd
 def load_data():
     with open("tasks.json", "r", encoding="utf-8") as f:
         data = json.load(f)
@@ -16,7 +21,7 @@ def pasek(tasks,etykiety):
         time.sleep(0.005)
     licznik.metric(label=etykiety, value=tasks)
     pasek.progress(1.0)
-def libraries():
+def libraries(show=True):
     tasks = load_data()
     numpy_tasks = [z for z in tasks if "numpy" in z['solve'].lower()]
     amount_n=len(numpy_tasks)
@@ -24,9 +29,21 @@ def libraries():
     amount_p=len(pandas_tasks)
     scikit_lear_tasks = [z for z in tasks if "sklearn" in z['solve'].lower()]
     amount_s=len(scikit_lear_tasks)
-    pasek(amount_n,"Zadania z wykorzystaniem Numpy")
-    pasek(amount_p, "Zadania z wykorzystaniem Pandas")
-    pasek(amount_s, "Zadania z wykorzystaniem Scikit-Learn")
+    re_tasks = [z for z in tasks if "import re" in z['solve'].lower()]
+    amount_r = len(re_tasks)
+    os_tasks = [z for z in tasks if ' os' in z['solve'].lower()]
+    amount_os = len(os_tasks)
+    path_tasks = [z for z in tasks if 'pathlib' in z['solve'].lower()]
+    amount_path = len(path_tasks)
+
+    if show:
+        pasek(amount_n, "Zadania z wykorzystaniem Numpy")
+        pasek(amount_p, "Zadania z wykorzystaniem Pandas")
+        pasek(amount_s, "Zadania z wykorzystaniem Scikit-Learn")
+
+
+    return amount_p, amount_n, amount_s, amount_r, amount_os, amount_path
+
 
 
 def line_code():
@@ -54,6 +71,105 @@ def days():
     result=len(unique_dates)
     pasek(result, "Ilość dni")
 
+
+
+def labels():
+    p, n, s, re, os, path = libraries(show=False)
+    category=['Pandas', 'Numpy', 'SCLEARN', 'RE', 'OS', 'PATHLIB']
+    values=[p,n,s,re,os, path]
+
+    fig,ax=plt.subplots(nrows=2, ncols=2, figsize=(13,15))
+
+    bars=ax[0,0].bar(category, values, width=0.7 ,color='skyblue', edgecolor='black')
+    ax[0,0].bar_label(bars, padding=4, weight='bold')
+    ax[0,0].tick_params(axis='x', rotation=50)
+    ax[0,0].set_ylim(0, max(values)*1.1 )
+
+#drugi wykres
+    bars= ax[0,1].barh(category, values, color='green',edgecolor='black')
+    ax[0,1].bar_label(
+        bars,
+        label_type='center',
+        weight= 'bold',
+        fontsize=10
+    )
+#trzeci wykres
+    ax[1,0].plot(category, values, marker='o', linestyle='-', color='green')
+    for x,y in zip(category, values):
+        ax[1,0].text(x, y+2, str(y), ha='left', va='bottom', weight='bold')
+    ax[1,0].grid(True, linestyle='-', alpha=0.6, color='gray')
+
+#czwarty wykres
+    ax[1,1].scatter(category, values, color='blue')
+    ax[1,1].grid(True, linestyle='-', alpha=0.6, color='gray')
+    ax[1,1].invert_xaxis()
+    for x,y in zip(category, values):
+        ax[1,1].text(x,y+3, str(y), ha='center', weight='bold')
+
+    st.pyplot(fig)
+
+
+
+
+def methods():
+    all_words=[]
+    for item in tasks:
+        solve=item.get('solve', '')
+        words=re.findall(r'[a-z]+', solve)
+        all_words.extend(words)
+    counter=Counter(all_words)
+    top_words=counter.most_common(11)
+
+    category=[]
+    values=[]
+    for w, i in top_words:
+        category.append(w)
+        values.append(i)
+    df_words = pd.DataFrame({'Category': category, "Values": values})
+
+    #Klatki animacji
+    frames=[]
+    steps=200
+    for i in range(1, steps+1):
+        factor=i/steps
+        for word,val in zip(category, values):
+            current_val = int(val * factor)
+            frames.append({
+                'Category': word,
+                'Values': current_val,
+                'Frame': i,
+                'Text_Clean':str(current_val) if i==steps else ""
+            })
+    df_frames=pd.DataFrame(frames)
+
+    return df_words, df_frames
+
+
+def labels_with_plotly(data):
+    fig1=px.pie(data, names='Category', values='Values', title='1.Pie')
+    fig1.update_traces(hovertemplate='%{label}-%{value}<extra></extra>', pull=[0.09 if i==0 else 0 for i in range(len(data))]
+                       )
+    fig2=px.treemap(data, path=['Category'], values='Values', title='2.Tree Map')
+    fig2.update_traces(hovertemplate='%{percentRoot:.1%}<extra></extra>')
+    fig3=px.pie(data, names='Category', values='Values', hole=0.5, title='3.Donut')
+    fig4=px.bar(how_words_animated, x='Category', y='Values',range_y=[0,800], animation_frame='Frame', animation_group='Category',text='Text_Clean', title='4.Bar')
+    fig4.update_yaxes(tick0=0,
+                      dtick=50)
+    fig4.layout.updatemenus[0].buttons[0].args[1]["frame"]["duration"] = 0.02
+    fig4.layout.updatemenus[0].buttons[0].args[1]["transition"]["duration"] = 0.2
+    fig4.update_layout(hovermode=False)
+
+    col1, col2=st.columns(2)
+    with col1:
+        st.plotly_chart(fig1, use_container_width=True)
+    with col2:
+        st.plotly_chart(fig2, use_container_width=True)
+
+    col3, col4=st.columns(2)
+    with col3:
+        st.plotly_chart(fig3, use_container_width=True)
+    with col4:
+        st.plotly_chart(fig4, use_container_width=True)
 
 
 
@@ -86,10 +202,10 @@ Cały ten projekt to nie tylko rozwiązane zadania, ale też mały ekosystem kod
 
 ### A teraz przejdźmy do statystyk
 """)
-pasek(amount, "Ile zadań zrobiłem")
-libraries()
-line_code()
-days()
+#pasek(amount, "Ile zadań zrobiłem")
+#libraries(show=True)
+#line_code()
+#days()
 
 @st.fragment
 def przegladarka_zadan():
@@ -134,4 +250,9 @@ def przegladarka_zadan():
 
 st.write('###   Podgląd Zadań')
 przegladarka_zadan()
+st.write('###   Wykresy')
+labels()
+how_words=methods()[0]
+how_words_animated=methods()[1]
+labels_with_plotly(how_words)
 
